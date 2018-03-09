eda57ac1f8d41e4e00293cb4f68985dfffcaad8f
lauweiwei <viluu1997@gmail.com>
2018-03-09 19:22:46 +0700

    bai 1 va 2

---
+++ Lab01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bai 1: ");
+            string hostName = Dns.GetHostName();
+            Console.WriteLine("Ten may: " + hostName);
+            IPHostEntry ip = Dns.GetHostEntry(hostName);
+            int ipNo = 0;
+            foreach (IPAddress ipAdd in ip.AddressList)
+                Console.WriteLine("IP thu " + ++ipNo + " la: " + ipAdd.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Bai 2: ");
+            Console.Write("Nhap IP hoac ten mien: ");
+            string host = Console.ReadLine();
+            IPHostEntry domainName = Dns.GetHostEntry(host);
+            Console.WriteLine("Ten mien: " + domainName.HostName);
+            foreach (string s in domainName.Aliases)
+                Console.WriteLine("Bi danh: " + s);
+            foreach (IPAddress i in domainName.AddressList)
+                Console.WriteLine("IP: " + i);
+            Console.ReadLine();
+        }
+    }
+}
